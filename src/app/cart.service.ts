25024b30b67ab3b32ee17286202ee50a1b36a592
Queite <queitesc@gmail.com>
2023-08-09 20:22:17 -0300

    feat: :sparkles: implement cart methods to remove and updatecart items and calculate the total

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -7,6 +7,7 @@ import { IProductCart } from './products';
 export class CartService {
 
   items: IProductCart[] = [];
+  total = 0;
 
   constructor() { }
 
@@ -20,10 +21,28 @@ export class CartService {
     this.items.push(product);
     console.log(this.items);
     localStorage.setItem('cart', JSON.stringify(this.items));
+    this.calcTotal();
   }
 
   clearCart() {
     this.items = [];
-    localStorage.clear()
+    localStorage.clear();
+    this.total = 0;
+  }
+
+  removeCartItem(productId: number) {
+    this.items = this.items.filter(item => item.id !== productId);
+    localStorage.setItem('cart', JSON.stringify(this.items));
+    this.calcTotal();
+  }
+
+  calcTotal() {
+    this.total = this.items.reduce((acc, curr) => acc + curr.price * curr.purchaseQuantity, 0);
+  }
+
+  updateCartItem(productId: number, newQuantity: number) {
+    const index = this.items.findIndex(item => item.id === productId);
+    this.items[index].purchaseQuantity = newQuantity;
+    this.calcTotal();
   }
 }
--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -11,9 +11,21 @@ export class CartComponent implements OnInit {
 
   cartItems: IProductCart[] = [];
 
-  constructor(private cartService: CartService) {}
+  constructor(public cartService: CartService) {}
 
   ngOnInit(): void {
     this.cartItems = this.cartService.getCartItems();
+    this.cartService.calcTotal();
   }
-}
+
+  removeCartItem(productId: number) {
+    this.cartItems = this.cartItems.filter(item => item.id !== productId);
+    this.cartService.removeCartItem(productId);
+  }
+
+  updateCartItem(productId: number, newQuantity: number) {
+    const index = this.cartItems.findIndex(item => item.id === productId);
+    this.cartItems[index].purchaseQuantity = newQuantity;
+    this.cartService.updateCartItem(productId, newQuantity);
+  }
+ }
