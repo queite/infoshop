5f7c3fc767327f21c2cbf55a3cd86f35304d08d1
Queite <queitesc@gmail.com>
2023-08-09 14:52:43 -0300

    feat: :sparkles: add product-details component

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { ProductsRoutingModule } from './products-routing.module';
 import { ProductsComponent } from './products.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 
 @NgModule({
   declarations: [
-    ProductsComponent
+    ProductsComponent,
+    ProductDetailsComponent
   ],
   imports: [
     CommonModule,
